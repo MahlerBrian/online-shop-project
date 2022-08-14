4c39717b32c8c1914cf92f661f5a7492adf2e218
Brian Albert <brian.david.albert@gmail.com>
2022-08-14 13:57:42 -0400

    added root folders, static files, initial styling to login/auth pages

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require('path');
+
 const express = require('express');
 
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
-app.use(authRoutes);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
+
+app.use(authRoutes);
 
 app.listen(3000);
 
