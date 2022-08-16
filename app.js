b0811929188e328085e9ee1839d3509eb3e7951f
Brian Albert <brian.david.albert@gmail.com>
2022-08-16 17:07:42 -0400

    added user signup functions

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
 
 app.use(authRoutes);
 
 
-db.connectToDatabase().then(() => {
-    app.listen(3000);
-}).catch((error) => {
+db.connectToDatabase()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch((error) => {
     console.log('Failed to connect to the database.');
     console.log(error);
 });
