681d2cdc39c49be37ac9cd039f9703478650207f
Brian Albert <brian.david.albert@gmail.com>
2022-08-16 15:01:49 -0400

    Set up and connected mongodb database

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,11 +2,16 @@ function getSignup(req, res) {
     res.render('customer/auth/signup');
 }
 
+function signUp() {
+
+}
+
 function getLogin(req, res) {
     //...
 }
 
 module.exports = {
     getSignup: getSignup,
-    getLogin: getLogin
+    getLogin: getLogin,
+    signUp: signUp
 };
