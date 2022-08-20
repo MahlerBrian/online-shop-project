4dbb6c1adb633cc4ea77fe89d84587dea451dd55
Brian Albert <brian.david.albert@gmail.com>
2022-08-20 10:56:19 -0400

    added sessions middleware and logic; added subfolders to views folder

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const csrf = require('csurf');
+const expressSession = require('express-session');
 
-
+const createSessionConfig = require('./config/session');
 const db = require('./Data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 const authRoutes = require('./routes/auth.routes');
+
 
 const app = express();
 
@@ -16,11 +19,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
-
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
     .then(() => {
